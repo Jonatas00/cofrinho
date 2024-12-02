8f55d19d31d90a52d746fe1f871768d15c2db4f0
Jonatas <djonatas31@gmail.com>
2024-12-02 12:04:23 -0300

    .

--- src/CofrinhoMain.java
+++ src/CofrinhoMain.java
@@ -10,9 +10,30 @@ public class CofrinhoMain {
     Cofrinho cofre = new Cofrinho();
 
     Scanner sc = new Scanner(System.in);
-
+    System.out.println("Escolha uma opção: ");
     menu.opcoes();
 
+    boolean sair = false;
+    while (!sair) {
+      String input = sc.nextLine();
+      switch (input) {
+        case "1":
+          menu.adicionarMoeda();
+          break;
+        case "2":
+          // todo
+        case "3":
+          // todo
+        case "4":
+          // todo
+        case "0":
+          sair = true;
+          break;
+        default:
+          System.out.println("Digite uma opção válida!");
+      }
+    }
     sc.close();
+    System.out.println("Fechando o cofrinho...");
   }
 }
--- src/Model/Moedas/Euro.java
+++ src/Model/Moedas/Euro.java
@@ -1,5 +1,19 @@
 package Model.Moedas;
 
-public class Euro {
+import Model.Moeda;
+
+public class Euro extends Moeda {
+  public Euro(double valor) {
+    super(valor);
+  }
+
+  public double converter() {
+    return valor * 6.38;
+  }
+
+  public void info() {
+    System.out.println("Euro:" + valor);
+
+  }
 
 }
