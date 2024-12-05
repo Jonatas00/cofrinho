b197b1c96c1e020a2afda9ffd9a9b7f74b5bb6f7
Jonatas <djonatas31@gmail.com>
2024-12-05 09:01:51 -0300

    .

--- src/Model/Menu.java
+++ src/Model/Menu.java
@@ -2,7 +2,13 @@ package Model;
 
 public class Menu {
 
+  private void limparTerminal() {
+    // Limpa a tela do console usando sequência de escape ANSI
+    System.out.print("\033\143");
+  }
+
   public void opcoes() {
+    System.out.println("Aluno: Jonatas Rodrigues Damasceno - RU: 4589297");
     System.out.println("COFRINHO BANKS:");
     System.out.println("1 - Adicionar moeda");
     System.out.println("2 - Remover moeda");
@@ -12,9 +18,16 @@ public class Menu {
   }
 
   public void adicionarMoeda() {
+    limparTerminal();
     System.out.println("Escolha a moeda:");
     System.out.println("1 - Real");
     System.out.println("2 - Dolar");
     System.out.println("3 - Euro");
+  }
+
+  public void encerrar() throws InterruptedException {
+    System.out.println("Encerrando sessão...");
+    Thread.sleep(1000);
+    System.out.println("Sessão encerrada!");
   }
 }
